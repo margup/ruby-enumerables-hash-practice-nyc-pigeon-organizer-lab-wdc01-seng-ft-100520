34f54b1b6ce037b3f7d36cda5a02899af4fc246d
bright-lifecycle-9875 <mairgup@icloud.com>
2020-09-22 01:13:02 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,16 +4,16 @@ def nyc_pigeon_organizer(data)
     #binding.pry
     value.each do |inner_key,names|
       names.each do |name|
-        binding.pry
+        #binding.pry
         if !final_array[name]
           final_array[name] = {}
        end
        if !final_array[name][key]
          !final_array[name][kay] = []
        end
-       final_array[name][key].push(inner_key)
+       final_array[name][key].push(inner_key.to_s)
       end
     end  
   end
-  binding.pry
+  
 end
