c85dbbede82bb6ed255d84003f0f30ed67882bef
bright-lifecycle-9875 <mairgup@icloud.com>
2020-09-22 01:17:40 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,21 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  # binding.pry
+  data.each_with_object({}) do |(key, value), new_hash|
+    value.each { |inner_key, inner_values|
+      # binding.pry
+      inner_values.each {|name|
+        if !new_hash[name]
+          new_hash[name] = {}
+        end
+        if !new_hash[name][key]
+          new_hash[name][key] = []
+        end
+        new_hash[name][key].push(inner_key.to_s)
+        # binding.pry
+      }
+    }
+    # binding.pry
+    new_hash
+  end
 end
